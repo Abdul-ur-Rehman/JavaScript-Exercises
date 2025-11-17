013780fcc96c626781aaa6c88a1e5883dd447eab
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-11-17 18:02:31 +0500

    largestnum

--- LargestOfTwoIntegers.js
+++ LargestOfTwoIntegers.js
@@ -1,1 +1,10 @@
-console.log("HelloWorld")
+var num1 = 5.9, num2 = 10
+
+if (num1 > num2){
+
+    console.log("The lagest number is "+ num1)
+}
+else{
+
+    console.log("The largest number is "+ num2)
+}
