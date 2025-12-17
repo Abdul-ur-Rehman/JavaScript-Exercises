e91d2bd09259a2e32c0e4af6c71fce5893afee59
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-12-17 12:18:08 +0500

    dates and mac test

---
+++ Dates.js
@@ -0,0 +1,9 @@
+const now = new Date(1998,8,18);
+
+
+now.setMonth(1)
+
+console.log(now)
+
+console.log(`Day of the week: ${now.getDay()}`)
+console.log(`Day of the week: ${now.getDate()}`)
---
+++ test1.js
@@ -0,0 +1,13 @@
+test = 'Hello World!';
+test1 = 20
+console.log(`${test}`);
+console.log(test1.parseInt)
+num1 = 50
+console.log(parseInt(`${1 + 1}`))
+
+console.log(typeof(num1.toString()))
+
+console.log(
+
+    "test"
+)
